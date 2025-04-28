e44a5bbb9c631d1c8e70118bc14628d88b9b98a0
Anton Yashchenko <zhymet@gmail.com>
2025-04-28 17:34:40 -0400

    - Split minitest2 module documentation into author and user folder to seperate concerns. - Added config file Doxyfile.in to handle documentation settings. - Added doxydoc.h to configure doxygen documentation topics and additional docs related to this module. - Started (partially unfinished) user guide for cmake-minitest integration using the updated minitest2 api. - Moved minitest modules readme.md to top of module folder structure (preparing to make it a consumable git package). - Updated the minitest module main CMakeLists.txt. There is still some work to  be done :  to turn it into a xxxxConfig.cmake findable package.     - Added unit tests which validate the minitest library.     - Set up doxygen package ajnd configuration file.

---
+++ modules/minitest2/docs/doxydoc.h
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright 2025 Anton Yashchenko
+// Licensed under the GNU Affero General Public License, Version 3.
+///////////////////////////////////////////////////////////////////////////////
+// Project: Minitest Library
+// Author(s): Anton Yashchenko
+// Website: https://www.acpp.dev
+///////////////////////////////////////////////////////////////////////////////
+/// @file
+/// Project-wide doxygen documentation settings/pre-definitions.
+///////////////////////////////////////////////////////////////////////////////
+
+// Topics, lists of all topics with brief descriptions and hierarchy:
+
+///////////////////////////////////////////////////////////////////////////////
+// Groups
+///////////////////////////////////////////////////////////////////////////////
+
+/// @defgroup minitest Minitest Framework
+/// Minimal C++ unit testing framework.
+  
+  /// @defgroup minitest0_macro_api User API
+  /// @ingroup minitest   
+  /// User's public interface.
+    
+    /// @defgroup minitest0_macro_api0_case_definitions Case Definitions
+    /// @ingroup minitest0_macro_api
+    /// Macros for defining test cases.
+
+    /// @defgroup minitest0_macro_api1_checks_and_asserts Checks and Asserts
+    /// @ingroup minitest0_macro_api
+    /// Macros for checking and asserting within test cases.
+
+  /// @defgroup minitest2_framework_impl Framework Implementation
+  /// @ingroup minitest
+  /// Minitest framework implementation details.
+  
+  /// @defgroup minitest3_common Common Defs and Utilities
+  /// @ingroup minitest
+  /// Common definitions and utilities used in the library.
+  
+  /// @defgroup minitest4_unittest Library Tests
+  /// @ingroup minitest  
+  /// Documentation for tests performed to validate this library.
