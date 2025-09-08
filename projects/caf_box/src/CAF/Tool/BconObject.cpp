3391e36ed32d4bd655ce5adfa24d43d05960dfad
Anton Yashchenko <zhymet@gmail.com>
2025-09-08 00:16:16 -0400

    + caf : add sources for `CAF/Tool` -> `BconObject.cpp/hpp`, `Stopwatch.cpp/hpp` , `Timeline.cpp/hpp`, `Timer.cpp/hpp`

--- projects/caf_box/src/CAF/Tool/BconObject.cpp
+++ projects/caf_box/src/CAF/Tool/BconObject.cpp
@@ -1,43 +1,48 @@
-///////////////////////////////////////////////////////////////////////////////
-// @project: Sophia Silicon Gold Solutions
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright 2025 Anton Yashchenko
+// Licensed under the GNU Affero General Public License, Version 3.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// @project: [CAF] Core Application Framework
 // @author(s): Anton Yashchenko
 // @website: https://www.acpp.dev
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// @file
-/// @ingroup cppmodule5_coreappfw
-/// @brief caf_cache_file.hpp source
-///////////////////////////////////////////////////////////////////////////////
+/// @ingroup coreappfw
+/// @brief [SOURCE] BconObject class
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// @addtogroup cppmodule5_coreappfw
+/// @addtogroup coreappfw
 /// @{
 
-#include "caf_cache_file.hpp"
+// clang-format off
+#include "CAF/Tool/BconObject.hpp"
 
-#include <cxxx.hpp>
+#include <filesystem>
+#include <map>
+#include <vector>
+#include <string>
+
+#include "cxxx.hpp"
+// clang-format on
 
 namespace caf {
-bool CacheFile::ContainsTag(const std::string& tag) const {
-  return live_cache.contains(tag);
-}
+bool BconObject::ContainsTag(const std::string& tag) const { return live_cache.contains(tag); }
 
-bool CacheFile::ContainsLineByTagAndIndex(const std::string& tag,
-                                          std::size_t index) {
+bool BconObject::ContainsLineByTagAndIndex(const std::string& tag, std::size_t index) {
   if (live_cache.contains(tag)) {
     return index < live_cache[tag].size();
   }
   return false;
 }
 
-const std::vector<std::string>& CacheFile::GetLinesByTag(
-    const std::string& tag) {
+const std::vector<std::string>& BconObject::GetLinesByTag(const std::string& tag) {
   if (live_cache.contains(tag)) {
     return live_cache[tag];
   }
   return {};
 }
 
-std::vector<cxx::stdfs::path> CacheFile::GetLinesByTagAsPath(
-    const std::string& tag) {
+std::vector<cxx::stdfs::path> BconObject::GetLinesByTagAsPath(const std::string& tag) {
   std::vector<cxx::stdfs::path> ret{};
   if (live_cache.contains(tag)) {
     for (const auto& entry : live_cache[tag]) {
@@ -47,62 +52,56 @@ std::vector<cxx::stdfs::path> CacheFile::GetLinesByTagAsPath(
   return ret;
 }
 
-const std::string& CacheFile::GetLineByTagAndIndex(const std::string& tag,
-                                                   std::size_t index) {
+const std::string& BconObject::GetLineByTagAndIndex(const std::string& tag, std::size_t index) {
   if (live_cache.contains(tag)) {
     if (index < live_cache[tag].size()) return live_cache[tag][index];
   }
   return "";
 }
 
-cxx::BoolError CacheFile::ReplaceLinesByTag(
-    const std::string& tag, const std::vector<std::string>& new_lines) {
+cxx::BoolError BconObject::ReplaceLinesByTag(const std::string& tag, const std::vector<std::string>& new_lines) {
   if (live_cache.contains(tag)) {
     live_cache[tag].clear();
-    live_cache[tag].insert(live_cache[tag].end(), new_lines.begin(),
-                           new_lines.end());
+    live_cache[tag].insert(live_cache[tag].end(), new_lines.begin(), new_lines.end());
     return true;
   }
-  return "No chache lines with this tag exist";
+  return "No cache lines with this tag exist";
 }
 
-cxx::BoolError CacheFile::ReplaceLineByTagAndIndex(
-    const std::string& tag, std::size_t index, const std::string& new_line) {
+cxx::BoolError BconObject::ReplaceLineByTagAndIndex(const std::string& tag, std::size_t index,
+                                                    const std::string& new_line) {
   if (live_cache.contains(tag)) {
     if (index < live_cache[tag].size()) {
       live_cache[tag][index] = new_line;
       return true;
     }
   }
-  return "No chache lines with this tag exist";
+  return "No cache lines with this tag exist";
 }
 
-void CacheFile::PushLinesToLiveCache(const TaggedLineMapNodeVec& file_nodes) {
+void BconObject::PushLinesToLiveCache(const TaggedLineMapNodeVec& file_nodes) {
   for (const auto& nd : file_nodes) {
     PushLinesToLiveCache(nd.key(), nd.mapped());
   }
 }
 
-void CacheFile::PushLinesToLiveCache(const std::string& tag,
-                                     const std::vector<std::string>& lines) {
+void BconObject::PushLinesToLiveCache(const std::string& tag, const std::vector<std::string>& lines) {
   if (live_cache.contains(tag)) {
     live_cache[tag].insert(live_cache[tag].end(), lines.begin(), lines.end());
   } else
     live_cache[tag] = lines;
 }
 
-void CacheFile::RemoveLiveCacheLinesByTag(const std::string& tag) {
-  live_cache.erase(tag);
-}
+void BconObject::RemoveLiveCacheLinesByTag(const std::string& tag) { live_cache.erase(tag); }
 
-void CacheFile::PopLineFromLiveCacheByTag(const std::string& tag) {
+void BconObject::PopLineFromLiveCacheByTag(const std::string& tag) {
   if (live_cache.contains(tag)) {
     live_cache[tag].pop_back();
   }
 }
 
 // Loads entire file into the live cache.
-void CacheFile::LoadLinesToLiveCache() {
+void BconObject::LoadLinesToLiveCache() {
   std::ifstream in_file_stream{file_path};
   std::string line;
   while (std::getline(in_file_stream, line)) {
@@ -119,15 +118,14 @@ void CacheFile::LoadLinesToLiveCache() {
 
       // Append std::vectors if the tag already exists.
       if (live_cache.contains(tag)) {
-        live_cache[tag].insert(live_cache[tag].end(), lines.begin(),
-                               lines.end());
+        live_cache[tag].insert(live_cache[tag].end(), lines.begin(), lines.end());
       } else
         live_cache[tag] = lines;
     }
   }
 }
 
-void CacheFile::SaveLiveCacheToFile() {
+void BconObject::SaveLiveCacheToFile() {
   std::ofstream out_file_stream{file_path};
   for (const auto& [tag, lines] : live_cache) {
     out_file_stream << "#def#\n" << tag << "\n";
@@ -138,27 +136,25 @@ void CacheFile::SaveLiveCacheToFile() {
   }
 }
 
-void CacheFile::ClearLiveCache() { live_cache.clear(); }
+void BconObject::ClearLiveCache() { live_cache.clear(); }
 
-void CacheFile::RefreshCacheFile() {
+void BconObject::RefreshCacheFile() {
   std::ofstream cache_fstream(file_path);
   cache_fstream.clear();
   cache_fstream.close();
 }
 
-bool CacheFile::DoesFileExist() const { return cxx::stdfs::exists(file_path); }
+bool BconObject::DoesFileExist() const { return cxx::stdfs::exists(file_path); }
 
-CacheFile::CacheFile(const cxx::stdfs::path& file_path)
-    : file_path(file_path) {}
+BconObject::BconObject(const cxx::stdfs::path& file_path) : file_path(file_path) {}
 
-void CacheFile::PushLinesToFile(const TaggedLineMapNodeVec& file_nodes) {
+void BconObject::PushLinesToFile(const TaggedLineMapNodeVec& file_nodes) {
   for (const auto& nd : file_nodes) {
     PushLinesToFile(nd.key(), nd.mapped());
   }
 }
 
-void CacheFile::PushLinesToFile(const std::string& tag,
-                                const std::vector<std::string>& lines) {
+void BconObject::PushLinesToFile(const std::string& tag, const std::vector<std::string>& lines) {
   // 1 line for #def# directive
   // 1 line for #end# directive
   // 1 line for the tag.
@@ -170,16 +166,18 @@ void CacheFile::PushLinesToFile(const std::string& tag,
   }
   out_file_stream << "#end#\n";
 }
+
 }  // namespace caf
 
-/// @} // end of cppmodule5_coreappfw
+/// @} // end of coreappfw
 
-///////////////////////////////////////////////////////////////////////////////
-// @project: Sophia Silicon Gold Solutions
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// @project: [CAF] Core Application Framework
 // @author(s): Anton Yashchenko
 // @website: https://www.acpp.dev
-///////////////////////////////////////////////////////////////////////////////
-// Copyright 2024 Anton Yashchenko
+// @created: 2025/05/09
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright 2025 Anton Yashchenko
 //
 // Licensed under the GNU Affero General Public License, Version 3.
 // you may not use this file except in compliance with the License.
@@ -192,4 +190,4 @@ void CacheFile::PushLinesToFile(const std::string& tag,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ projects/caf_box/src/CAF/Tool/Timer.cpp
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright 2025 Anton Yashchenko
+// Licensed under the GNU Affero General Public License, Version 3.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// @project: [CAF] Core Application Framework
+// @author(s): Anton Yashchenko
+// @website: https://www.acpp.dev
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file
+/// @ingroup coreappfw
+/// @brief [HEADER] Timer class
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// @addtogroup coreappfw
+/// @{
+
+// clang-format off
+#include "CAF/Tool/Timer.hpp"
+#include <chrono>
+// clang-format on
+
+namespace caf {
+
+[[nodiscard]] Timer::MicroDurationType Timer::ElapsedTime() const { return ClockType::now() - reset_point_; };
+
+[[nodiscard]] Timer::MicroDurationType Timer::LastResetPoint() const { return reset_point_.time_since_epoch(); };
+
+Timer::MicroDurationType Timer::Reset() {
+  auto ret_duration = ElapsedTime();
+  reset_point_ = ClockType::now();
+  return ret_duration;
+}
+
+Timer& Timer::Restart() {
+  reset_point_ = ClockType::now();
+  return *this;
+}
+
+}  // namespace caf
+
+/// @} // end of coreappfw
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// @project: [CAF] Core Application Framework
+// @author(s): Anton Yashchenko
+// @website: https://www.acpp.dev
+// @created: 2025/05/09
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright 2025 Anton Yashchenko
+//
+// Licensed under the GNU Affero General Public License, Version 3.
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
