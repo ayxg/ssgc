439a3259beab082a49e55f9f45f46c2b1ab091f3
Anton Yashchenko <zhymet@gmail.com>
2026-01-16 21:18:41 -0500

    + `cnd` : template specialization for `void` type for `ExpectedTypeInterface`

--- projects/cnd_box/inc/cldev/clmsg.hpp
+++ projects/cnd_box/inc/cldev/clmsg.hpp
@@ -28,6 +28,7 @@ namespace cnd {
 
 // Type used by the compiler upon a translation failure. A buffer of dynamically allocated error messages.
 using CompilerProcessError = cldev::clmsg::ClMsgBuffer;
+
 // Purpose of this class is to provide an indirection layer from std::expected. Since the old implementation was
 // pre-C++23, the C++Extended Library [cxx::Expected] type was used. This allows easier migration of the code to C++23.
 template <class T>
@@ -46,10 +47,31 @@ struct ExpectedTypeInterface : public Ex<T, CompilerProcessError> {
   constexpr T& Value() noexcept { return this->value(); }
   constexpr const CompilerProcessError& Error() const noexcept { return this->error(); }
   constexpr const T& Value() const noexcept { return this->value(); }
+
+};
+
+template<>
+struct ExpectedTypeInterface<void> : public Ex<void, CompilerProcessError> {
+  using ValueT = void;
+  using ErrorT = CompilerProcessError;
+  using ImplT = Ex<ValueT, ErrorT>;
+  using ImplT::expected;  // use the constructor provided by std::expected.
+  using ImplT::operator=;
+  using Failure = Unex<ErrorT>;
+  using Success = ExpectedTypeInterface;
+  constexpr operator ImplT() const { return static_cast<ImplT>(*this); }
+
+  constexpr CompilerProcessError& Error() noexcept { return this->error(); }
+  constexpr const CompilerProcessError& Error() const noexcept { return this->error(); }
 };
 
 using cldev::clmsg::ClMsgBuffer;
 using cldev::clmsg::MakeClMsg;
+
+template <class T>
+using ClRes = ExpectedTypeInterface<T>;
+using ClFail = Unex<ClMsgBuffer>;
+
 template <class T>
 using CompilerProcessResult = ExpectedTypeInterface<T>;
 using CompilerProcessFailure = Unex<ClMsgBuffer>;
